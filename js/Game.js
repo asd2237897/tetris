768cfdc8327138aaa45575d76a0e115e4c76ccdc
天才小牛蛙 <243304742@qq.com>
2019-06-11 22:16:05 +0800

    完成样式

--- js/Game.js
+++ js/Game.js
@@ -1,42 +1,10 @@
 // 启动游戏
+//每个正方形30*30,一横10个
 ;
 (function (w) {
-    function Game(nextForm) {
-        this.nextForm = nextForm
-        this.Form1 = new Form1()
-        this.Form2 = new Form2()
-        this.Form3 = new Form3()
-        this.Form4 = new Form4()
-        this.Form5 = new Form5()
-        this.Form6 = new Form6()
-        this.Form7 = new Form7()
+    function Game(){
+
     }
-    w.Game = Game;
-    Game.prototype.start = function () {
-        var num = Math.ceil(Math.random() * 7)
-        switch (num) {
-            case 1:
-                this.Form1.show(this.nextForm);
-                break;
-            case 2:
-                this.Form2.show(this.nextForm)
-                break;
-            case 3:
-                this.Form3.show(this.nextForm)
-                break;
-            case 4:
-                this.Form4.show(this.nextForm)
-                break;
-            case 5:
-                this.Form5.show(this.nextForm)
-                break;
-            case 6:
-                this.Form6.show(this.nextForm)
-                break;
-            case 7:
-                this.Form7.show(this.nextForm)
-                break;
-        }
-        
-    }
+    w.Game = Game
+
 }(window))
