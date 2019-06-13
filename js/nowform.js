497c2ec4eb103c5d87fcbd81aada001e4f7a6f2b
天才小牛蛙 <243304742@qq.com>
2019-06-13 12:14:51 +0800

    判断能否继续左右移动

--- js/nowform.js
+++ js/nowform.js
@@ -1,32 +1,51 @@
 //创建新的图形方法
 ;
 (function (w) {
-    var list2 = [];
-    function NowForm(game) {
-        this.game = game;
-        
+    function NowForm() {
+        //偏移坐标x,y
+        this.x = 0;
+        this.y = 0;
+        //保存当前图形4个小方块的数组
+        this.list2 = [];
+        //记录每次新图形的坐标
+        this.ref = [];
     }
     w.NowForm = NowForm;
     NowForm.prototype.show = function(arrForm,now,pixel,nowColor){
-        for (var i = 0; i < list2.length; i++) {
-            document.querySelector('.bigDiv').removeChild(list2[i]);
-        }
-        list2 = [];
-        var smallArr = arrForm[now].split(",")
+        var smallArr = arrForm[now].split(",");
+        this.ref = smallArr;
         for (var i = 0; i < 8; i += 2) {
-            var nowDiv = document.createElement('div')
-            nowDiv.className = 'nextDiv'
+            var nowDiv = document.createElement('div');
+            nowDiv.className = 'nextDiv';
             nowDiv.style.width = (pixel - 2) + 'px';
             nowDiv.style.height = (pixel - 2) + 'px';
             nowDiv.style.left = (smallArr[i] * pixel + 90) + 'px';
             nowDiv.style.top = (smallArr[i + 1] * pixel + 30) + 'px';
-            nowDiv.style.backgroundColor = nowColor
+            nowDiv.style.backgroundColor = nowColor;
             // console.log(smallArr[i+1]);
-            document.querySelector('.bigDiv').appendChild(nowDiv)
-            list2.push(nowDiv)
+            document.querySelector('.bigDiv').appendChild(nowDiv);
+            this.list2.push(nowDiv);
+            
         }
+        console.log(this.list2);
     }
     NowForm.prototype.moveLeft = function(){
-        
+        this.x -=1;
+        if(){
+            for(var i = 0; i < this.list2.length; i++){
+                var left = parseInt(this.list2[i].style.left)
+                this.list2[i].style.left = (left - 30) +'px'
+            }
+        }else{
+            break;
+        }
     }
+    NowForm.prototype.moveRight = function(){
+        this.x +=1;
+        for(var i = 0; i < this.list2.length; i++){
+            var left = parseInt(this.list2[i].style.left)
+            this.list2[i].style.left = (left + 30) +'px'
+        }
+    }
+
 }(window))
